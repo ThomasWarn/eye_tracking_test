c24711a955fac8624950b22426d2ea3d312b4121
Thomas <thomaswarnasch@gmail.com>
2023-10-01 00:34:28 -0500

    .

--- EyeTracking_01.py
+++ EyeTracking_01.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy
 
 def verify_camera_works(camera_obj):
     #input: camera object.
@@ -8,6 +9,28 @@ def verify_camera_works(camera_obj):
     return ret
 
 def grab_frame(camera_obj):
+    ret, frame = camera_obj.read()
+    return frame
+
+def find_eye(frame, face_hc, eye_hc):
+    #input: frame.
+    #returns: cropped image of eye.
+
+    #crop image to head to minimize risk of false eyes.
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("test", gray_frame)
+    cv2.waitKey(1)
+    #print(gray_frame)
+    faces = face_cascade.detectMultiScale(gray_frame, 1.5, 5)
+    print(faces)
+    return False, False, False
+
+def find_pupil(eye_clip):
+    #input: face cropped
+    #returns: pupil locations.
+    pass
+
+def generate_direction(eye_center, pupil_center):
     pass
 
 def perform_calibration_via_matrix():
@@ -15,9 +38,12 @@ def perform_calibration_via_matrix():
 
 if __name__ == "__main__":
     camera_index = 0
-
+    face_haarcascade_filename = "haarcascade_frontalface_default.xml"
+    eye_haarcascade_filename = "haarcascade_eye.xml"
     #don't edit anything after this line.
     camera_obj = cv2.VideoCapture(camera_index)
+    face_hc = cv2.CascadeClassifier(cv2.data.haarcascades + face_haarcascade_filename)
+    eye_hc = cv2.CascadeClassifier(cv2.data.haarcascades + eye_haarcascade_filename)
     if not verify_camera_works(camera_obj):
         for i in range(10):#tries first 10 indexes for working camera.
             camera_obj = cv2.VideoCapture(camera_index)
@@ -27,6 +53,6 @@ if __name__ == "__main__":
             
     for i in range(100000): #not using a while true loop during development.
         frame = grab_frame(camera_obj)
-        eye_box, eye_center, eye_clip = find_eye(frame)
+        eye_box, eye_center, eye_clip = find_eye(frame, face_hc, eye_hc)
         pupil_center = find_pupil(eye_clip)
         generate_direction(eye_center,pupil_center)
